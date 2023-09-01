9d7fcf003ae14b62fd137de127a22078586db471
Nitesh Mistry <9368802+NiteshMistry@users.noreply.github.com>
2023-09-01 14:01:42 -0700

    Update IntegerToEnglishWords.java

--- IntegerToEnglishWords.java
+++ IntegerToEnglishWords.java
@@ -11,6 +11,10 @@ Output: "Twelve Thousand Three Hundred Forty Five"
 
 Input: num = 1234567
 Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+
+
+Time complexity: O(log n)
+Space complexity: O(log n)
  */
 
 public class IntegerToEnglishWords {
