b5a1104899f5eb2501160934d52c64f9aa6df981
Nitesh Mistry <9368802+NiteshMistry@users.noreply.github.com>
2023-09-01 13:34:52 -0700

    Create IntegerToEnglishWords.java

---
+++ IntegerToEnglishWords.java
@@ -0,0 +1,54 @@
+/*
+273. Integer to English Words
+
+Convert a non-negative integer num to its English words representation.
+
+Input: num = 123
+Output: "One Hundred Twenty Three"
+
+Input: num = 12345
+Output: "Twelve Thousand Three Hundred Forty Five"
+
+Input: num = 1234567
+Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+ */
+
+public class IntegerToEnglishWords {
+  private final String[] belowTen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+  private final String[] belowTwenty = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+  private final String[] belowHundred = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+  public String numberToWords(int num) {
+    if(num == 0)
+      return "Zero";
+    return helper(num);
+  }
+
+  public String helper(int num) {
+    String result;
+    if (num < 10)
+      result = belowTen[num];
+    else if (num < 20)
+      result = belowTwenty[num - 10];
+    else if (num < 100)
+      result = belowHundred[num / 10] + " " + belowTen[num % 10];
+    else if (num < 1000)
+      result = helper(num / 100) + " Hundred " + helper(num % 100);
+    else if (num < 1000000)
+      result = helper(num / 1000) + " Thousand " + helper(num % 1000);
+    else if (num < 1000000000)
+      result = helper(num / 1000000) + " Million " + helper(num % 1000000);
+    else
+      result = helper(num / 1000000000) + " Billion " + helper(num % 1000000000);
+    return result.trim();
+  }
+
+  public static void main(String[] args) {
+    IntegerToEnglishWords integerToEnglishWords = new IntegerToEnglishWords();
+    System.out.println(integerToEnglishWords.numberToWords(123));
+    System.out.println(integerToEnglishWords.numberToWords(12345));
+    System.out.println(integerToEnglishWords.numberToWords(1234567));
+  }
+}
+
+
